c197c2120a46a36ad8a41873ce673a333afd7cbe
NathanielAAndres <johnnathaniel.andres@smc.pshs.edu.ph>
2026-02-07 15:34:47 +0800

    Add files via upload
    This python code asks for the user's input on their name, age, and favorite subject, stores it in a dictionary, and displays it as an array.

---
+++ Andres_Q3_FA06.py
@@ -0,0 +1,10 @@
+student = {}
+
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+favesub = input("Enter your favorite subject:")
+
+student["Name"] = name
+student["Age"] = age
+student["Favorite Subject"] = favesub
+print("Student Record:",student)
